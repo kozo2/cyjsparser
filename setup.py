a80bf4191a46c3872b69b9c20017ae6f5ef24244
Kozo Nishida <knishida@riken.jp>
2015-08-09 10:18:58 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cyjsparser',
+      version='0.1.0',
+      description='Cytoscape.js JSON (cyjs) parser for Python',
+      author='Kozo Nishida',
+      author_email='knishida@riken.jp',
+      url='https://github.com/kozo2/cyjsparser',
+      packages=['cyjsparser'],
+     )
